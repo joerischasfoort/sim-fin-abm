be232095ca12d0a2702c450796c5f4454a86d7da
joerischasfoort <joerischasfoort@gmail.com>
2018-05-22 17:42:13 +0200

    decoupling fundamentals

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from numpy import cumsum, log, polyfit, sqrt, std, subtract
+from numpy.random import randn
 
 def div0(a, b):
     """
@@ -10,3 +11,21 @@ def div0(a, b):
     if not np.isfinite(answer):
         answer = 0
     return answer
+
+
+def hurst(ts):
+	"""
+	source: https://www.quantstart.com/articles/Basics-of-Statistical-Mean-Reversion-Testing
+	Returns the Hurst Exponent of the time series vector ts
+	"""
+	# Create the range of lag values
+	lags = range(2, 100)
+
+	# Calculate the array of the variances of the lagged differences
+	tau = [sqrt(std(subtract(ts[lag:], ts[:-lag]))) for lag in lags]
+
+	# Use a linear fit to estimate the Hurst Exponent
+	poly = polyfit(log(lags), log(tau), 1)
+
+	# Return the Hurst exponent from the polyfit output
+	return poly[0]*2.0
