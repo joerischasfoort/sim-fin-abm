78220f11cf1c89cefd8c1c8ac1a4495e6f1c63c9
joerischasfoort <joerischasfoort@gmail.com>
2018-05-23 14:46:00 +0200

    improved replication notebook

--- simulation.py
+++ simulation.py
@@ -7,20 +7,20 @@ from simfinmodel import *
 parameters = {
     # global parameters
     "n_traders": 5000,
-    "trader_sample_size": 3,
-    "ticks": 1000,
+    "ticks": 20,
+    "trader_sample_size": 4,
     "fundamental_value": 100,
     "std_fundamental": 0.08,
-    "std_noise": 0.05,
+    "std_noise": 0.15,
     "std_vol": 4,
     "max_order_expiration_ticks": 50,
     # trader parameters
     "w_fundamentalists": 0.0,
-    "w_momentum": 80.0,
-    "w_random": 20.0,
-    "w_mean_reversion": 0.0,
+    "w_momentum": 0.0,
+    "w_random": 10.0,
+    "w_mean_reversion": 80.0,
     "w_buy_hold": 10.0,
-    "spread_max": 0.15, #TODO investigae if this is correct & change in paper
+    "spread_max": 0.05, # TODO investigate if this spread is correct
     "horizon_min": 2,
     "horizon_max": 8,
 }
