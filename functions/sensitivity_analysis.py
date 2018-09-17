60100afee58be840dfd3bdab8672c51fdf896276
joerischasfoort <joerischasfoort@gmail.com>
2018-09-17 17:15:09 +0200

    updated_notebooks

--- functions/sensitivity_analysis.py
+++ functions/sensitivity_analysis.py
@@ -10,14 +10,14 @@ from functions.helpers import *
 def simulate_params_sobol(NRUNS, parameter_set, fixed_parameters):
     """
     Simulate the simfin model for a set of changing and fixed parameters
-    :param stylized_fact: string parameter. Either: 'autocorrelation', 'kurtosis', 'autocorrelation_abs', 'hurst' or 'av_dev_from_fund'
     :param NRUNS: integer amount of Monte Carlo simulations
-    :param population: list of parameters which have been sampled for Sobol sensitivity analysis
+    :param parameter_set: list of parameters which have been sampled for Sobol sensitivity analysis
     :param fixed_parameters: list of parameters which will remain fixed
     :return: numpy array of average stylized facts outcome values for all parameter combinations
     """
-    stylized_facts = {'autocorrelation': [], 'kurtosis': [], 'autocorrelation_abs': [],
-                      'hurst': [], 'av_dev_from_fund': []}
+
+    # stylized facts order: first_order_autocors, autocors1, autocors5, mean_abs_autocor, kurtosis, spy_abs_auto10, spy_abs_auto25, spy_abs_auto50, spy_abs_auto100, cointegrations
+    stylized_facts = [[],[],[],[],[],[],[],[],[],[]]
 
     for parameters in parameter_set:
         # combine individual parameters with fixed parameters
@@ -34,30 +34,44 @@ def simulate_params_sobol(NRUNS, parameter_set, fixed_parameters):
         # store simulated stylized facts
         mc_prices, mc_returns, mc_autocorr_returns, mc_autocorr_abs_returns, mc_volatility, mc_volume, mc_fundamentals = organise_data(
             obs)
-        mc_dev_fundamentals = (mc_prices - mc_fundamentals) / mc_fundamentals
 
-        mean_autocor = []
-        mean_autocor_abs = []
-        mean_kurtosis = []
-        long_memory = []
-        av_deviation_fundamental = []
+        first_order_autocors = []
+        autocors1 = []
+        autocors5 = []
+        mean_abs_autocor = []
+        kurtoses = []
+        spy_abs_auto10 = []
+        spy_abs_auto25 = []
+        spy_abs_auto50 = []
+        spy_abs_auto100 = []
+        cointegrations = []
         for col in mc_returns:
-            mean_autocor.append(np.mean(mc_autocorr_returns[col][1:]))  # correct?
-            mean_autocor_abs.append(np.mean(mc_autocorr_abs_returns[col][1:]))
-            mean_kurtosis.append(mc_returns[col][2:].kurtosis())
-            long_memory.append(hurst(mc_prices[col][2:]))
-            av_deviation_fundamental.append(np.mean(mc_dev_fundamentals[col][1:]))
+            first_order_autocors.append(autocorrelation_returns(mc_returns[col][1:], 25))
+            autocors1.append(mc_returns[col][1:].autocorr(lag=1))
+            autocors5.append(mc_returns[col][1:].autocorr(lag=5))
+            mean_abs_autocor.append(autocorrelation_abs_returns(mc_returns[col][1:], 25))
+            kurtoses.append(mc_returns[col][2:].kurtosis())
+            spy_abs_auto10.append(mc_returns[col][1:].abs().autocorr(lag=10))
+            spy_abs_auto25.append(mc_returns[col][1:].abs().autocorr(lag=25))
+            spy_abs_auto50.append(mc_returns[col][1:].abs().autocorr(lag=50))
+            spy_abs_auto100.append(mc_returns[col][1:].abs().autocorr(lag=100))
+            cointegrations.append(cointegr(mc_prices[col][1:], mc_fundamentals[col][1:])[0])
 
-        stylized_facts['autocorrelation'].append(np.mean(mean_autocor))
-        stylized_facts['kurtosis'].append(np.mean(mean_kurtosis))
-        stylized_facts['autocorrelation_abs'].append(np.mean(mean_autocor_abs))
-        stylized_facts['hurst'].append(np.mean(long_memory))
-        stylized_facts['av_dev_from_fund'].append(np.mean(av_deviation_fundamental))
+        stylized_facts[0].append(np.mean(first_order_autocors))
+        stylized_facts[1].append(np.mean(autocors1))
+        stylized_facts[2].append(np.mean(autocors5))
+        stylized_facts[3].append(np.mean(mean_abs_autocor))
+        stylized_facts[4].append(np.mean(kurtoses))
+        stylized_facts[5].append(np.mean(spy_abs_auto10))
+        stylized_facts[6].append(np.mean(spy_abs_auto25))
+        stylized_facts[7].append(np.mean(spy_abs_auto50))
+        stylized_facts[8].append(np.mean(spy_abs_auto100))
+        stylized_facts[9].append(np.mean(cointegrations))
 
     return stylized_facts
 
 
-def m_core_sim_run(parameters):
+def m_core_sim_run(parameters): #TODO update
     """
     Run the simulation once with a set of parameters. Can be used to run on multiple cores
     :param NRUNS: integer number of runs to simulate the model for one instance of parameters
